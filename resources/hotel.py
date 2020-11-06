8fb8c58641e243299f32609e11a7c69de51a4af6
Gustavo Mendes <gustavoomendesoliveira@gmail.com>
2020-11-06 01:43:35 -0300

    Criando classe para hoteis

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,25 +1,25 @@
 from flask_restful import Resource, Api, reqparse
-
+from models.hotel import HotelModel
 hoteis =[
     {
         'hotel_id': '1', 
         'nome': 'Teste1',
         'estrelas': 4.3, 
-        'diraria': 380.90,
+        'diaria': 380.90,
         'cidade' : 'Rio de janeiro'
     },
     {
         'hotel_id': '2', 
         'nome': 'Teste2',
         'estrelas': 4.3, 
-        'diraria': 380.90,
+        'diaria': 380.90,
         'cidade' : 'Rio de janeiro'
     },
     {
         'hotel_id': '3', 
         'nome': 'Teste3',
         'estrelas': 4.3, 
-        'diraria': 380.90,
+        'diaria': 380.90,
         'cidade' : 'Rio de janeiro'
     }
 ]
@@ -34,7 +34,7 @@ class Hotel(Resource):
     argumentos = reqparse.RequestParser()
     argumentos.add_argument('nome')
     argumentos.add_argument('estrelas')
-    argumentos.add_argument('diraria')
+    argumentos.add_argument('diaria')
     argumentos.add_argument('cidade')
 
     def findHotelById(hotel_id):
@@ -51,22 +51,16 @@ class Hotel(Resource):
 
     def post(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        novo_hotel = {
-            'hotel_id' :hotel_id,
-            'nome': dados['nome'],
-            'estrelas': dados['estrelas'],
-            'diraria': dados['diraria'],
-            'cidade': dados['cidade'],
-        }
-
+        hotel_objeto = HotelModel(hotel_id,**dados)
+        novo_hotel = hotel_objeto.json()
         hoteis.append(novo_hotel)
         return novo_hotel, 200
 
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        novo_hotel = {'hotel_id' :hotel_id,**dados}
-
+        hotel_objeto = HotelModel(hotel_id,**dados)
+        novo_hotel = hotel_objeto.json()
         hotel = Hotel.findHotelById(hotel_id)
 
         if hotel:
@@ -78,5 +72,8 @@ class Hotel(Resource):
         
 
     def delete(self, hotel_id):
-        pass
+        global hoteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id']!= hotel_id]
+        print(hoteis)
+        return {'message':'Hotel deletado.'}
 
