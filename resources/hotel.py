8d6a82c568c7fcf80d1f937db770f5a2bd492bcd
Gustavo Mendes <gustavoomendesoliveira@gmail.com>
2020-11-09 00:21:54 -0300

    Adicionando metodos GET e POST

--- resources/hotel.py
+++ resources/hotel.py
@@ -37,31 +37,28 @@ class Hotel(Resource):
     argumentos.add_argument('diaria')
     argumentos.add_argument('cidade')
 
-    def findHotelById(hotel_id):
-        for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
-                return hotel
-        return None
-
+    
     def get(self, hotel_id):
-        hotel = Hotel.findHotelById(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            return hotel
+            return hotel.json()
         return{'message': 'Hotel not found.'},404
 
     def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {"message": "Hotel id '{}' alredy exists.".format(hotel_id)},400
+
         dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id,**dados)
-        novo_hotel = hotel_objeto.json()
-        hoteis.append(novo_hotel)
-        return novo_hotel, 200
+        hotel = HotelModel(hotel_id,**dados)
+        hotel.save_hotel()
+        return hotel.json()
 
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
         hotel_objeto = HotelModel(hotel_id,**dados)
         novo_hotel = hotel_objeto.json()
-        hotel = Hotel.findHotelById(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
 
         if hotel:
             hotel.update(novo_hotel)
